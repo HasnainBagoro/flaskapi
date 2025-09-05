d679716ab9e2d37659032f15d32d6a80ed09db3a
Muhammad Hasnain Bagoro <mhassuu143@gmail.com>
2025-09-05 16:43:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 import uvicorn
 
@@ -7,15 +8,17 @@ app = FastAPI()
 # Load trained model
 model = joblib.load("url_model_tldfreq.pkl")
 
+class URLInput(BaseModel):
+    url: str
+
 @app.get("/")
 def home():
     return {"message": "Malicious URL Detection API is running"}
 
 @app.post("/predict")
-def predict_url(url: str):
-    # Example: you can add preprocessing of URL here
-    prediction = model.predict([url])[0]
-    return {"url": url, "malicious": bool(prediction)}
+def predict_url(data: URLInput):
+    prediction = model.predict([data.url])[0]
+    return {"url": data.url, "malicious": bool(prediction)}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
